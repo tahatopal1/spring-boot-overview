05a0470d5535c63524ec070454fd11f04eabfb95
tahatopal <taha.topal.7@gmail.com>
2025-11-09 03:14:45 +0300

    new commit

---
+++ src/main/java/com/project/springoverview/SpringoverviewApplication.java
@@ -0,0 +1,13 @@
+package com.project.springoverview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringoverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringoverviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/springoverview/SpringoverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springoverview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringoverviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
